a7317fcb32082f8532c962d35a1a278ff211f376
SKCwillie <rwilliams651@gmail.com>
2023-08-12 14:32:19 -0500

    changing response structure

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,36 @@ class AircraftSerializer(serializers.ModelSerializer):
         fields = ['MakeModel', 'Crew', 'Seats', 'CruiseSpeed', 'GPH', 'FuelType', 'MTOW', 'EmptyWeight', 'Price',
                   'Ext1', 'LTip', 'LAux', 'LMain', 'Center1', 'Center2', 'Center3', 'RMain', 'RAux', 'RTip', 'RExt2',
                   'Engines', 'EnginePrice', 'ModelId', 'MaxCargo']
+
+    def to_representation(self, instance):
+        Fuel = dict()
+        Fuel['GPH'] = instance.GPH
+        Fuel['Type'] = instance.FuelType
+        Fuel['Ext1'] = instance.Ext1
+        Fuel['LTip'] = instance.LTip
+        Fuel['LAux'] = instance.LAux
+        Fuel['LMain'] = instance.LMain
+        Fuel['Center1'] = instance.Center1
+        Fuel['Center2'] = instance.Center2
+        Fuel['Center3'] = instance.Center3
+        Fuel['RMain'] = instance.RMain
+        Fuel['RAux'] = instance.RAux
+        Fuel['RTip'] = instance.RTip
+        Fuel['RExt2'] = instance.RExt2
+        Weight = dict()
+        Weight['Max'] = instance.MTOW
+        Weight['Empty'] = instance.EmptyWeight
+
+        representation = {
+            'id': instance.ModelId,
+            'makeModel': instance.MakeModel,
+            'Seats': instance.Seats,
+            'CruiseSpeed': instance.CruiseSpeed,
+            'Price': instance.Price,
+            'Engines': instance.Engines,
+            'EnginePrice': instance.EnginePrice,
+            'Fuel': Fuel,
+            'Weight': Weight
+        }
+
+        return representation
--- api/views.py
+++ api/views.py
@@ -4,6 +4,6 @@ from .serializers import AircraftSerializer
 
 
 def aircraft_list(request):
-    aircraft = Aircraft.objects.all()
+    aircraft = Aircraft.objects.all().order_by('ModelId')
     serializer = AircraftSerializer(aircraft, many=True)
     return JsonResponse(serializer.data, safe=False)
